e9fe501d3af3138b2aa5a3016972e5ece37028c7
Sekai Alien <sekai.alien@mail.ru>
2022-11-21 20:11:40 +0500

    task 41 - positive numbers in user's array

---
+++ task-41/Program.cs
@@ -0,0 +1,22 @@
+﻿// Задача 41: Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+
+// 0, 7, 8, -2, -2 -> 2
+// 1, -7, 567, 89, 223-> 3
+
+
+Console.WriteLine("Сколько будет чисел?");
+int m = Convert.ToInt32(Console.ReadLine());
+int sum = 0;
+int[] array = new int[m];
+
+Console.WriteLine("Введите числа:");
+
+for (int i = 0; i < m; i++)
+{
+    array[i] = Convert.ToInt32(Console.ReadLine());
+    if (array[i] > 0) { sum++; }
+}
+
+Console.WriteLine();
+Console.WriteLine(String.Join(", ", array));
+Console.WriteLine("Количество положительных чисел = " + sum);
